2bde21eaadb1402cab370d912e00f201c2eeacf4
Yagiz Kanbur <150202055@kocaeli.edu.tr>
2021-07-24 13:45:10 +0300

    Kafka implemented

--- src/main/java/com/grupc/userms/services/UserService.java
+++ src/main/java/com/grupc/userms/services/UserService.java
@@ -1,5 +1,6 @@
 package com.grupc.userms.services;
 
+import com.alibaba.fastjson.JSON;
 import com.grupc.userms.entities.User;
 import com.grupc.userms.exception.EmptyInputException;
 import com.grupc.userms.exception.UniqueFieldException;
@@ -11,18 +12,27 @@ import com.grupc.userms.repositories.UserRepository;
 import com.sun.istack.NotNull;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.kafka.core.KafkaTemplate;
+import com.grupc.userms.configuration.KafkaProducerConfiguration;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
+    private static final String UPDATE_TOPIC = "user.update";
+    private static final String CREATE_TOPIC = "user.create";
+    private static final String DELETE_TOPIC = "user.delete";
+
     private final UserRepository userRepository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, KafkaTemplate<String, String> kafkaTemplate) {
         this.userRepository = userRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public User addUser(CreateUserRequest request) throws UserException {
@@ -32,6 +42,7 @@ public class UserService {
         checkEmailUnique(request.getEmail());
 
         User createdUser = new User(request.getFullName(), request.getEmail());
+        kafkaTemplate.send(CREATE_TOPIC, JSON.toJSONString(createdUser, false));
         return userRepository.save(createdUser);
     }
 
@@ -39,6 +50,7 @@ public class UserService {
         User userToDelete = getUserById(user.getId());
         if (userToDelete != null) {
             userRepository.delete(user);
+            kafkaTemplate.send(DELETE_TOPIC, JSON.toJSONString(user, false));
             return;
         }
         throw new UserNotFoundException();
@@ -57,7 +69,7 @@ public class UserService {
         if (StringUtils.isNotBlank(request.getFullName())) {
             updatedUser.setFullName(request.getFullName());
         }
-
+        kafkaTemplate.send(UPDATE_TOPIC, JSON.toJSONString(updatedUser, false));
         return userRepository.save(updatedUser);
     }
 
@@ -69,6 +81,7 @@ public class UserService {
         }
 
         updatedUser.setFullName(request.getFullName());
+        kafkaTemplate.send(UPDATE_TOPIC, JSON.toJSONString(updatedUser, false));
         return userRepository.save(updatedUser);
     }
 
